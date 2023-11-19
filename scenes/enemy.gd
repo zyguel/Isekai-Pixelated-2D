238e5205ad6280874d9ca937cdc11b024c1c51db
zyguelusc <123135343+zyguel@users.noreply.github.com>
2023-11-20 00:19:20 +0800

    update speed & animation

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -1,13 +1,17 @@
 extends CharacterBody2D
 
-var speed = 60
+var speed = 48
 var player_chase = false
 var player = null
+
+func _ready():
+	$AnimatedSprite2D.play("idle")
 
 func _physics_process(delta):
 	if player_chase:
 		position += (player.position - position)/speed
-
+		$AnimatedSprite2D.play("walk")
+		
 func _on_detection_area_body_entered(body):
 	player = body
 	player_chase = true
